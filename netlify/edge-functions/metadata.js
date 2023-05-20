93d4e291a2b5ad3e06b94d6afbdf9a366f66fc18
Nicholas Jitkoff <nicholas@jitkoff.com>
2023-05-20 09:20:21 -0700

    Updated background colors, support relative urls

--- netlify/edge-functions/metadata.js
+++ netlify/edge-functions/metadata.js
@@ -5,21 +5,33 @@ function decodePrettyComponent(s) {
 
 function decodeURL(s) {
   if (s.startsWith("http")) return s;
-  return atob(s.replace(/=/g,''));
+  if (s.startsWith(".")) return s;
+  if (s.startsWith("/")) return s;
+  try {
+    return atob(s.replace(/=/g,''))
+  } catch (e) {
+    return s;
+  }
 }
 
 function atou(b64) { return decodeURIComponent(escape(atob(b64))); }
 function utoa(data) { return btoa(unescape(encodeURIComponent(data))); }
 
+let urlValues = ["u","i","v","f"];
 function pathToMetadata(path) {
   let components = path.substring(1).split("/");
-  let info = {title: decodePrettyComponent(components.shift())}
+  components.unshift("t"); // Title designation for the first element
+  let info = {}
   for (let i = 0; i < components.length; i+=2) {
     let key = components[i];
     let value = components[i+1];
     if (!value) continue;
-    if (key == "d") { value = decodePrettyComponent(value); }
-    else if (value.includes("%")) { value = decodeURIComponent(value); }
+    if (urlValues.includes(key)) {
+      value = decodeURIComponent(value);
+      if (value.startsWith(":")) value = "https://" + value.substring(1);
+    } else {
+      value = decodePrettyComponent(value);
+    }
     if (key.length && value.length) info[key] = value;
   }
   return info;
@@ -56,15 +68,31 @@ export default async (request, context) => {
         let info = pathToMetadata(path)
 
         let content = ['<meta charset="UTF-8">'];
-        if (info.title) { content.push(`<title>${info.title}</title>`,mProp("og:title", info.title)) }
+        if (info.t) { content.push(`<title>${info.t}</title>`,mProp("og:title", info.t)) }
         if (info.s) { content.push(mProp("og:site_name", info.s)) }
-        if (info.t) { content.push(mProp("og:type", info.t)) }
+        if (info.y) { content.push(mProp("og:type", info.y)) }
         if (info.d) { content.push(mProp("og:description", info.d),mName("description",info.d)) }
         if (info.c) { content.push(mName("theme-color","#" + info.c)) }
 
+        if (info.u) {
+          info.u = decodeURL(info.u)
+          if (!info.u.startsWith("http")) info.u = "https://" + info.u;
+          content.push(mProp("og:url", info.u));
+          // content.push(`<script>location.href="${info.u}"</script>`);
+        } else {
+          // content.push(`<script>l=location;l.href=l.hash.substring(1)||'//www.'+l.host</script>`);
+        }
+        
         if (info.i) {
           info.i = decodeURL(info.i)
-          if (!info.i.startsWith("http")) info.i = "/.netlify/functions/rasterize/" + info.i
+          if (info.i.startsWith("svg:")) {
+            info.i = "/.netlify/functions/rasterize/" + info.i;
+          } else if (info.u && (info.i.startsWith(".") || info.i.startsWith("/"))) {
+            info.i = new URL(info.i, info.u).href
+          } else {
+            info.i = "https://" + info.i;
+          }
+
           content.push(mProp("og:image", info.i)); 
           if (info.iw) content.push(mProp("og:image:width", info.iw)); 
           if (info.ih) content.push(mProp("og:image:width", info.ih)); 
@@ -77,21 +105,15 @@ export default async (request, context) => {
         } 
         if (info.f) { // Favicon: URL Encoded
           if (info.f.length > 9){      
-            content.push(`<link rel="icon" type="image/png" href="${decodeURL(info.f)}">`);
+            content.push(`<link rel="icon" type="image/png" href="${decodeURL(new URL(info.f, info.u).href)}">`);
           } else {
             let codepoints = Array.from(info.f).map(c => c.codePointAt(0).toString(16));
             content.push(`<link rel="icon" type="image/png" href="https://fonts.gstatic.com/s/e/notoemoji/14.0/${codepoints.join("_")}/128.png">`);
           }
         }
 
-        if (info.u) {
-          info.u = decodeURL(info.u)
-          content.push(mProp("og:url", info.u));
-          content.push(`<script>location.href="${info.u}"</script>`);
-        } else {
-          content.push(`<script>l=location;l.href=l.hash.substring(1)||'//www.'+l.host</script>`);
-        }
-        // content.push(mProp("og:url", request.url)`);
+
+      
         console.log(["Metadata Request", JSON.stringify(info), geo, ua].join('\t')); 
         return new Response(content.join("\n"), {
           headers: { "content-type": "text/html" },
